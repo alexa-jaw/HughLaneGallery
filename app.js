34687ba6b98d81ec9eabcb65f7fc178508fa11e7
Pragya <pragyavash14@gmail.com>
2021-03-30 17:24:06 +0100

    trying to add re-usable footer

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ function topFunction(){
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//ToDo Pragya: Trying to implement re-usable footer
+$(document).ready(function(){
+  $('#footer').load('components/footer.html');
+});
